9f4bf53efa6b466db4f915a42183c5f7beb24bf7
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-10-09 13:44:15 -0600

    cambios de prueba

--- src/config/configdatabase.js
+++ src/config/configdatabase.js
@@ -3,13 +3,13 @@ const sql = require('mssql');
 
 // Configura la cadena de conexión a SQL Server
 const config = { //creamos un objeto con las propiedadades de la coneccio
-  user: 'tu_usuario',
-  password: 'tu_contraseña',
-  server: 'tu_servidor', // nombre del servidor
-  database: 'gestordepagos',
-  options: {
-    encrypt: true, // Para conexiones seguras (solo si estás utilizando SSL)
-  },
+    user: '',
+    password: '',
+    server: 'localhost', // nombre del servidor
+    database: 'gestordepagos',
+    options: {
+        encrypt: true, // Para conexiones seguras (solo si estás utilizando SSL)
+    },
 };
 
 // Crea un objeto de pool de conexiones
@@ -17,11 +17,11 @@ const pool = new sql.ConnectionPool(config);
 
 // Conecta al servidor SQL Server
 pool.connect()
-  .then(() => {
-    console.log('Conexión exitosa a SQL Server.');
-  })
-  .catch((err) => {
-    console.error('Error de conexión a SQL Server:', err);
-  });
+    .then(() => {
+        console.log('Conexión exitosa a SQL Server.');
+    })
+    .catch((err) => {
+        console.error('Error de conexión a SQL Server:', err);
+    });
 
 module.exports = pool;
