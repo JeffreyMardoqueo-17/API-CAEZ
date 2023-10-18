7ac0451efedca87b643b27b3e2b7a6480d8a7e60
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-10-18 11:46:46 -0600

    cambios de la parte del controler

--- src/controllers/Documentos.controller.js
+++ src/controllers/Documentos.controller.js
@@ -6,17 +6,25 @@ export const GetDocumentos = async (req, res) => {
     //consulta a la base de datos 
     try {
         const pool = await GetConnection();
-        const result = await pool.request().query('SELECT * FROM TipoDocumento')
-        console.log(result)
+        const result = await pool.request().query('SELECT * FROM TipoDocumento') //consulta
+        console.log(result)//muestro por cosola el result
         res.json(result.recordset)
     } catch (error) {
         console.log(`Hay errores y es:${error}`);
     }
 };
 //No se que es lo que pasa  
-export const CreateNewDocumento = async (req, res)=>{
+export const CreateNewDocumento = async (req, res) => {
 
-    const {name, description} = await req.body
-    console.log(name, description)
-    res.json('new document')
+    try {
+        const { name, description } = await req.body //req resive o muestras cosas
+        if (name == null || description == null) {
+            return res.status(400).json({ msg: 'Heey papu por favor llena todos los campos' })
+        } else if (name == null) {
+            return res.status(400).json()
+        }
+        res.json(`New Document ${name} ${description}`)
+    } catch (error) {
+        console.log(`Los errores estan en: ${error}`)
+    }
 }
