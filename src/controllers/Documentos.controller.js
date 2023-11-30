b8b14069742f296dfcb15464684d2228f4bcfad5
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-11-30 13:59:10 -0600

    metodo post

--- src/controllers/Documentos.controller.js
+++ src/controllers/Documentos.controller.js
@@ -2,12 +2,12 @@
 import { GetConnection } from "../database/conection";
 import sql from 'mssql';
 
-const pool = await GetConnection(); //coneccion 
-export const GetDocumentos = async (req, res) => {
+//==================================Obtener documento=========
 
+export const GetDocumentos = async (req, res) => {
     //consulta a la base de datos 
     try {
-        // const pool = await GetConnection();
+        const pool = await GetConnection();
         const result = await pool.request().query('SELECT * FROM TipoDocumento') //consulta
         console.log(result)//muestro por cosola el resultado
         res.send('todo bien, resive esto, esto es el metodo Get y esta GOOOD ¡Buena we!')
@@ -17,32 +17,34 @@ export const GetDocumentos = async (req, res) => {
         console.log(`Hay errores y es:${error}`);
     }
 };
-
-//---Tabla Tipo Documento
-// CREATE TABLE TipoDocumento(
-// Id TINYINT NOT NULL PRIMARY KEY IDENTITY (1,1),
-// Nombre VARCHAR(80) NOT NULL,
-// )
+//=================================METODO PARA CREAR UN DOCUMENTO====================
 export const CreateNewDocumento = async (req, res) => {
     try {
-        const { name } = req.body; //esto es para recirbir ddel DOM
+        const { name } = req.body;
 
         if (!name) {
-            return res.status(400).json({ msg: 'Por favor, llena todos los campos requeridos (nombre).'});
+            return res.status(400).json({ msg: 'Por favor, llena todos los campos requeridos (nombre).' });
         }
-        // Define los parámetros para el SP
+
+        const pool = await GetConnection();
+        const requestCheck = new sql.Request();
+        const checkQuery = `SELECT COUNT(*) AS count FROM TuTabla WHERE Nombre = @Nombre`; // Replace 'TuTabla' with your actual table name
+
+        const checkResult = await requestCheck.input('Nombre', sql.VarChar(80), name).query(checkQuery);
+
+        if (checkResult.recordset[0].count > 0) {
+            return res.status(400).json({ msg: 'El tipo de documento ya existe.' });
+        }
+
         const request = new sql.Request();
-        const result = await pool.request().input('Nombre', sql.VarChar(80), name);
-        // Ejecuta el SP
-        await request.execute('SPInsertarTipoDoc');
-        res.send('todo bien, resive esto, esto es el metodo Get y esta GOOOD ¡Buena we!')
-        res.json(result.recordset)
-        console.log(result)
-        res.status(201).json({ msg: 'Nuevo tipo de documento creado con éxito.' });
+        const result = await request.input('Nombre', sql.VarChar(80), name).execute('SPInsertarTipoDoc');
+
+        res.json({ msg: 'Nuevo tipo de documento creado con éxito.', result: result.recordset });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ msg: 'Se produjo un error al procesar la solicitud' });
     }
-}
+};
+
 //metodo de modificar
 //metodo de eliminar
