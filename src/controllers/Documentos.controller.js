707630df3907c9e3460941d8ce7df918ed3417c2
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-11-30 08:00:04 -0600

    seccion new document

--- src/controllers/Documentos.controller.js
+++ src/controllers/Documentos.controller.js
@@ -15,23 +15,6 @@ export const GetDocumentos = async (req, res) => {
         console.log(`Hay errores y es:${error}`);
     }
 };
-//No se que es lo que pasa  
-// export const CreateNewDocumento = async (req, res) => {
-
-//     try {
-//         const { name, description } = await req.body //req resive o muestras cosas
-//         if (name == null || description == null) {
-//             return res.status(400).json({ msg: 'Heey papu por favor llena todos los campos' })
-//         } else if (name == null) {
-//             return res.status(400).json()
-//         }
-//         console.log(name, description)
-//         res.json(`New Document ${name} ${description}`)
-//         res.send(`New Document, estamos en el metodo post <br><br> ${name} ${description}`)
-//     } catch (error) {
-//         console.log(`Los errores estan en: ${error}`)
-//     }
-// }
 export const CreateNewDocumento = async (req, res) => {
     try {
         const { name } = req.body; //esto es para recirbir ddel DOM
