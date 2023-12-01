e186e4f8149211fa35f54fe8f98e20a1eaf3c8e3
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-01 12:48:28 -0600

    secion create de tipoo pago

--- src/routers/TipoPago.routes.js
+++ src/routers/TipoPago.routes.js
@@ -1,9 +1,9 @@
 import {Route, Router} from "express"; //router permitira crear rutas URLS
-import {GetTipoPago} from '../controllers/TipoPago.controller'
+import {GetTipoPago, POSTNewTypePayment} from '../controllers/TipoPago.controller'
 
 const router = Router();
 //creamos la ruta para traer
 router.get('/TipePago', GetTipoPago) //la ruta mas la funcion  asincrona
-
+router.get(`/TypepaymentCreate`,POSTNewTypePayment )
 
 export default router;//exportamos el router
