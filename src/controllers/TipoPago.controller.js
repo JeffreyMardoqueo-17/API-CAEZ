642abbdc52a1067fa6b337af44cf3853d627b0f1
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-01 12:02:48 -0600

    tipo pago routes

---
+++ src/controllers/TipoPago.controller.js
@@ -0,0 +1,16 @@
+
+import { GetConnection } from "../database/conection";
+import sql from 'mssql';
+
+export const GetTipoPago = async (req, res) =>{
+    /*Esto consultaea a la BD tipo: Select * FROM TipoPago */
+    try{
+        const pool = await GetConnection(); //llamo a lo que importe
+        const result = await pool.request().query(`SELECT * FROM TipoPago`) //ESTO ES PARA HACER LA CONSULTA
+        console.log(result)//aqui se mostrara el resultado en consola
+        res.send(`Si funciona y esta es la respuesta de la consulta: ${result}`)
+        res.json(result.recordset)
+    } catch (error){
+        console.log(`Valio, si hay errore y es en: ${error}`)
+    }
+}
---
+++ src/routers/TipoPago.routes.js
@@ -0,0 +1,4 @@
+import {Route, Router} from "express"; //router permitira crear rutas URLS
+import {GetTipoPago} from '../controllers/TipoPago.controller'
+
+const router = Route();
