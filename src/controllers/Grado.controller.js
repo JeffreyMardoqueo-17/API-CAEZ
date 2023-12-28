07021bb8c7593e300bde619255e0dd507c7ff720
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-28 12:33:27 -0600

    Revert "metodo insertar casi lsito"
    This reverts commit d2da53bde2f79297d873f6d9647a31eb6950d8ee.

--- src/controllers/Grado.controller.js
+++ src/controllers/Grado.controller.js
@@ -17,23 +17,17 @@ export const NewGrado = async (req, res) => {
     try {
         const { nombre } = req.body;
 
-        // Validar si 'nombre' es un string no vacío
-        if (typeof nombre !== 'string' || nombre.trim() === '') {
-            return res.status(400).json({ msg: 'BAD REQUEST. El campo "nombre" es inválido o está vacío.' });
+        if (!nombre) {
+            return res.status(400).json({ msg: 'BAD REQUEST. Por favor llena los datos correctamente' });
         }
 
         const pool = await GetConnection();
-        const result = await pool
+        await pool
             .request()
-            .input("Nombre", sql.VarChar(50), nombre)
+            .input("Nombre", sql.VarChar, nombre)
             .execute("SPInsertarGrado");
 
-        // Verificar si se afectaron filas en la inserción
-        if (result.rowsAffected && result.rowsAffected[0] > 0) {
-            res.status(201).json({ message: 'Nuevo grado agregado correctamente' });
-        } else {
-            res.status(500).json({ error: 'Error al insertar el registro en la base de datos' });
-        }
+        res.status(201).json({ message: 'Nuevo grado agregado correctamente' });
     } catch (error) {
         console.error(`Error al agregar nuevo grado: ${error.message}`);
         res.status(500).json({ error: 'Error al insertar el registro en la base de datos' });
